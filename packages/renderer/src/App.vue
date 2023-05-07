2d2fc06a0c6782d3191b83039499460a6896b3fe
Nathanael Björkgård <nathanael@bjorkgard.se>
2023-05-07 10:18:23 +0200

    Fix dark mode

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -3,14 +3,14 @@ import {onMounted} from 'vue';
 
 onMounted(() => {
   if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.getElementsByTagName('body')[0].classList.add('dark');
+    document.getElementsByTagName('html')[0].classList.add('dark');
   }
 
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', ({matches}) => {
     if (matches) {
-      document.getElementsByTagName('body')[0].classList.add('dark');
+      document.getElementsByTagName('html')[0].classList.add('dark');
     } else {
-      document.getElementsByTagName('body')[0].classList.remove('dark');
+      document.getElementsByTagName('html')[0].classList.remove('dark');
     }
   });
 });
--- packages/renderer/src/pages/PublishersList.vue
+++ packages/renderer/src/pages/PublishersList.vue
@@ -1,3 +1,15 @@
 <script setup lang="ts"></script>
 
-<template> Förkunnare </template>
+<template>
+  <div class="flex items-center mt-4">
+    <h2 class="mr-auto text-lg font-medium intro-y">Förkunnare</h2>
+  </div>
+  <!-- BEGIN: Wizard Layout -->
+  <div class="py-10 mt-5 intro-y box sm:py-20">
+    <div
+      class="relative before:hidden before:lg:block before:absolute before:w-[69%] before:h-[3px] before:top-0 before:bottom-0 before:mt-4 before:bg-slate-100 before:dark:bg-darkmode-400 flex flex-col lg:flex-row justify-center px-5 sm:px-20"
+    >
+      <div class="z-10 flex items-center flex-1 intro-x lg:text-center lg:block"> CONTENT </div>
+    </div>
+  </div>
+</template>
