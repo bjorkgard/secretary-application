2dcc3a55a86665cd2af4ece3600257593e0f64d1
Nathanael Björkgård <nathanael@bjorkgard.se>
2023-12-28 16:27:07 +0100

    feat: Confirm template type after upload

--- src/main/functions/importTemplate.ts
+++ src/main/functions/importTemplate.ts
@@ -3,6 +3,7 @@ import fs from 'fs-extra'
 import log from 'electron-log'
 import i18n from '../../localization/i18next.config'
 import { TemplateService } from '../../types/type'
+import { confirmTemplate } from '.'
 
 const isDevelopment = import.meta.env.MAIN_VITE_NODE_ENV !== 'production'
 
@@ -29,7 +30,32 @@ export default function importTemplate(
     if (!result.canceled) {
       try {
         fs.copyFileSync(result.filePaths[0], `${userTemplatePath}/${args.code}.pdf`)
-        templateService.upsert({ ...args, path: `${userTemplatePath}/${args.code}.pdf` })
+
+        confirmTemplate(args.code, `${userTemplatePath}/${args.code}.pdf`).then((confirmed) => {
+          if (confirmed) {
+            templateService.upsert({ ...args, path: `${userTemplatePath}/${args.code}.pdf` })
+
+            mainWindow.webContents.send('template-imported')
+
+            return dialog.showMessageBox(mainWindow, {
+              type: 'info' as const,
+              buttons: ['OK'],
+              defaultId: 0,
+              title: i18n.t('templates.imported'),
+              message: i18n.t('templates.importedMessage'),
+              detail: ''
+            })
+          } else {
+            return dialog.showMessageBox(mainWindow, {
+              type: 'info' as const,
+              buttons: ['OK'],
+              defaultId: 0,
+              title: i18n.t('templates.errorImported'),
+              message: i18n.t('templates.errorWrongFile'),
+              detail: ''
+            })
+          }
+        })
       } catch (err) {
         log.error(err)
         return dialog.showMessageBox(mainWindow, {
@@ -41,17 +67,6 @@ export default function importTemplate(
           detail: ''
         })
       }
-
-      mainWindow.webContents.send('template-imported')
-
-      return dialog.showMessageBox(mainWindow, {
-        type: 'info' as const,
-        buttons: ['OK'],
-        defaultId: 0,
-        title: i18n.t('templates.imported'),
-        message: i18n.t('templates.importedMessage'),
-        detail: ''
-      })
     }
 
     return null
