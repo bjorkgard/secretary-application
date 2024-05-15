5aed27c82544dae6b560cce64dcc18544d856c21
Nathanael Björkgård <nathanael@bjorkgard.se>
2024-05-15 10:50:46 +0200

    fix(menu): show the application menu

--- src/renderer/src/core/ErrorPage.tsx
+++ src/renderer/src/core/ErrorPage.tsx
@@ -4,17 +4,17 @@ export default function ErrorPage(): JSX.Element {
   const error: unknown = useRouteError()
   return (
     <div id="error-page" className="flex h-screen flex-col items-center justify-center gap-8">
-      <p className="text-slate-400">
-        <div
-          className="inline-block size-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-          role="status"
+      <div
+        className="inline-block size-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        role="status"
+      >
+        <span
+          className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
         >
-          <span
-            className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-          >
-            Loading...
-          </span>
-        </div>
+          Loading...
+        </span>
+      </div>
+      <p className="text-slate-400">
         <i>{(error as Error)?.message || (error as { statusText?: string })?.statusText}</i>
       </p>
     </div>
