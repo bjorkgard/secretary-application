98603b76934ba3c2e50b91954da91fceca9ef6c5
Nathanael Björkgård <nathanael@bjorkgard.se>
2024-05-15 10:18:32 +0200

    fix(loading): remove debug text

--- src/renderer/src/core/ErrorPage.tsx
+++ src/renderer/src/core/ErrorPage.tsx
@@ -2,13 +2,19 @@ import { useRouteError } from 'react-router-dom'
 
 export default function ErrorPage(): JSX.Element {
   const error: unknown = useRouteError()
-
-  // TODO: Show if error else a spinning loading icon
   return (
     <div id="error-page" className="flex h-screen flex-col items-center justify-center gap-8">
-      <h1 className="text-4xl font-bold">Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
       <p className="text-slate-400">
+        <div
+          className="inline-block size-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status"
+        >
+          <span
+            className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+          >
+            Loading...
+          </span>
+        </div>
         <i>{(error as Error)?.message || (error as { statusText?: string })?.statusText}</i>
       </p>
     </div>
