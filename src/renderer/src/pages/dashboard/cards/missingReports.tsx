a841da7aaca639d49cbd7ae8ca1f4f3813a8abde
Nathanael Björkgård <nathanael@bjorkgard.se>
2024-06-10 21:28:01 +0200

    feat(report reminder): resend email or copy the url to the report

--- src/renderer/src/pages/dashboard/cards/missingReports.tsx
+++ src/renderer/src/pages/dashboard/cards/missingReports.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState }                 from 'react'
-import { useTranslation }                      from 'react-i18next'
-import { Card }                                from '@renderer/components/Card'
-import type { ServiceMonthModel }              from 'src/types/models'
-import { useEffectOnce }                       from '@renderer/hooks/useOnMountUnsafe'
-import { DevicePhoneMobileIcon, EnvelopeIcon } from '@heroicons/react/20/solid'
+import { useEffect, useState }                                from 'react'
+import { ClipboardIcon, DevicePhoneMobileIcon, EnvelopeIcon } from '@heroicons/react/16/solid'
+import { useTranslation }                                     from 'react-i18next'
+import { Card }                                               from '@renderer/components/Card'
+import { useEffectOnce }                                      from '@renderer/hooks/useOnMountUnsafe'
+import { useSettingsState }                                   from '@renderer/store/settingsStore'
+import type { ServiceMonthModel }                             from 'src/types/models'
 
 interface missingReport {
   identifier: string
@@ -13,7 +14,8 @@ interface missingReport {
 }
 
 export default function MissingReports(): JSX.Element | null {
-  const { t } = useTranslation()
+  const { t }         = useTranslation()
+  const settingsState = useSettingsState()
 
   const [loading, setLoading]               = useState<boolean>(true)
   const [serviceMonth, setServiceMonth]     = useState<ServiceMonthModel>()
@@ -44,6 +46,39 @@ export default function MissingReports(): JSX.Element | null {
     })
   }, [serviceMonth])
 
+  const resendPublisherReport = (identifier: string) => {
+    window.electron.ipcRenderer.invoke('resend-publisher-report', { identifier }).then(() => {
+      window.Notification.requestPermission().then(() => {
+        // eslint-disable-next-line no-new
+        new window.Notification('SECRETARY', {
+          body:   t('reports.resend'),
+          silent: false,
+        })
+      })
+    })
+  }
+
+  const getReportUrl = (identifier: string) => {
+    window.electron.ipcRenderer.invoke('get-report-url', { identifier }).then((url) => {
+      window.Notification.requestPermission().then(() => {
+        if (url !== '') {
+          // eslint-disable-next-line no-new
+          new window.Notification('SECRETARY', {
+            body:   t('reports.urlCopied'),
+            silent: false,
+          })
+        }
+        else {
+          // eslint-disable-next-line no-new
+          new window.Notification('SECRETARY', {
+            body:   t('reports.noReportUrl'),
+            silent: true,
+          })
+        }
+      })
+    })
+  }
+
   if (!serviceMonth || serviceMonth?.status === 'DONE') {
     // The report for this month is DONE, so there are no missing reports
     return null
@@ -63,20 +98,34 @@ export default function MissingReports(): JSX.Element | null {
                   {missingReports.map(report => (
                     <tr key={report.identifier}>
                       <td>{report.name}</td>
-                      <td className="flex grow justify-end">
+                      <td className="flex grow justify-end space-x-2">
                         {report.email
-                          ? (
-                            <a href={`mailto:${report.email}`}>
-                              <EnvelopeIcon className="size-5" />
-                            </a>
-                            )
+                          ? settingsState.online.send_report_publisher
+                            ? (
+                              <span className="tooltip tooltip-left" data-tip={t('tooltip.resendReport')} onClick={() => resendPublisherReport(report.identifier)}>
+                                <EnvelopeIcon className="size-4" />
+                              </span>
+                              )
+                            : (
+                              <a className="tooltip tooltip-left" data-tip={t('tooltip.sendEmail')} href={`mailto:${report.email}`}>
+                                <EnvelopeIcon className="size-4" />
+                              </a>
+                              )
                           : null}
 
                         {report.mobile
                           ? (
-                            <a href={`sms:${report.mobile}?body=${t('sms.missingReport')}`}>
-                              <DevicePhoneMobileIcon className="size-5" />
+                            <a className="tooltip tooltip-left" data-tip={t('tooltip.sendReportMessage')} href={`sms:${report.mobile}?body=${t('sms.missingReport')}`}>
+                              <DevicePhoneMobileIcon className="size-4" />
                             </a>
+                            )
+                          : null}
+
+                        {settingsState.online.send_report_publisher
+                          ? (
+                            <span className="tooltip tooltip-left" data-tip={t('tooltip.copyReportUrl')} onClick={() => { getReportUrl(report.identifier) }}>
+                              <ClipboardIcon className="size-4" />
+                            </span>
                             )
                           : null}
                       </td>
