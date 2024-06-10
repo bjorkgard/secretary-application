e22b90ac5fb45fe384ba485fa08faae4f207a5fb
Nathanael Björkgård <nathanael@bjorkgard.se>
2024-06-10 22:39:13 +0200

    feat(events): add events for applications

--- src/main/functions/storeEvent.ts
+++ src/main/functions/storeEvent.ts
@@ -29,6 +29,26 @@ async function storeEvent(mainWindow: BrowserWindow, event: EventProps): Promise
   let removeFromActiveReports = false
 
   switch (event.command) {
+    case 'A-2':
+      information           = `${publisher?.firstname} ${publisher?.lastname}`
+      addEventToServiceYear = false
+      break
+    case 'A-8':
+      information           = `${publisher?.firstname} ${publisher?.lastname}`
+      addEventToServiceYear = false
+      break
+    case 'A-19':
+      information           = `${publisher?.firstname} ${publisher?.lastname}`
+      addEventToServiceYear = false
+      break
+    case 'CO-5A':
+      information           = `${publisher?.firstname} ${publisher?.lastname}`
+      addEventToServiceYear = false
+      break
+    case 'CO-4':
+      information           = `${publisher?.firstname} ${publisher?.lastname}`
+      addEventToServiceYear = false
+      break
     case 'BAPTISED':
       information = `${publisher?.firstname} ${publisher?.lastname}`
       publisher   = { ...publisher, baptised: event.date }
