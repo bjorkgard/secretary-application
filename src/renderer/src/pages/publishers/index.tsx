b35c20d7a7d562179e5a9e568b612a1ea8ed8e71
Nathanael Björkgård <nathanael@bjorkgard.se>
2025-01-21 00:48:49 +0100

    Sort publishers by column

--- src/renderer/src/pages/publishers/index.tsx
+++ src/renderer/src/pages/publishers/index.tsx
@@ -1,21 +1,26 @@
-import { useEffect, useState }                                                  from 'react'
-import { useTranslation }                                                       from 'react-i18next'
-import { useNavigate }                                                          from 'react-router-dom'
-import { MagnifyingGlassIcon, PlusIcon }                                        from '@heroicons/react/24/solid'
-import { EllipsisHorizontalIcon }                                               from '@heroicons/react/20/solid'
-import { DocumentArrowDownIcon, PencilIcon, PlusIcon as PlusIconSmall }         from '@heroicons/react/16/solid'
-import { formatPhoneNumber }                                                    from 'react-phone-number-input'
-import type { PublicCongregationModel, PublisherModel, ServiceGroupModel }      from 'src/types/models'
-import { isTemplateCorrect }                                                    from '@renderer/utils/isTemplateCorrect'
-import { Heading }                                                              from '@renderer/components/catalyst/heading'
-import { Button }                                                               from '@renderer/components/catalyst/button'
-import { InputGroupSmall, InputSmall }                                          from '@renderer/components/catalyst/input-small'
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow }        from '@renderer/components/catalyst/table'
-import { Badge }                                                                from '@renderer/components/catalyst/badge'
-import { TextLink }                                                             from '@renderer/components/catalyst/text'
-import { Dropdown, DropdownButton, DropdownHeader, DropdownItem, DropdownMenu } from '@renderer/components/catalyst/dropdown'
-import ROUTES                                                                   from '../../constants/routes.json'
-import EventModal                                                               from './components/eventModal'
+import { useEffect, useState }                                                           from 'react'
+import { useTranslation }                                                                from 'react-i18next'
+import { useNavigate }                                                                   from 'react-router-dom'
+import { MagnifyingGlassIcon, PlusIcon }                                                 from '@heroicons/react/24/solid'
+import { EllipsisHorizontalIcon }                                                        from '@heroicons/react/20/solid'
+import { ChevronDownIcon, DocumentArrowDownIcon, PencilIcon, PlusIcon as PlusIconSmall } from '@heroicons/react/16/solid'
+import { formatPhoneNumber }                                                             from 'react-phone-number-input'
+import type { PublicCongregationModel, PublisherModel, ServiceGroupModel }               from 'src/types/models'
+import { isTemplateCorrect }                                                             from '@renderer/utils/isTemplateCorrect'
+import { Heading }                                                                       from '@renderer/components/catalyst/heading'
+import { Button }                                                                        from '@renderer/components/catalyst/button'
+import { InputGroupSmall, InputSmall }                                                   from '@renderer/components/catalyst/input-small'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow }                 from '@renderer/components/catalyst/table'
+import { Badge }                                                                         from '@renderer/components/catalyst/badge'
+import { TextLink }                                                                      from '@renderer/components/catalyst/text'
+import { Dropdown, DropdownButton, DropdownHeader, DropdownItem, DropdownMenu }          from '@renderer/components/catalyst/dropdown'
+import clsx                                                                              from 'clsx'
+import ROUTES                                                                            from '../../constants/routes.json'
+import EventModal                                                                        from './components/eventModal'
+
+interface publisherTableData {
+  publishers: PublisherModel[]
+}
 
 export default function Publishers(): JSX.Element {
   const { t }    = useTranslation()
@@ -28,6 +33,11 @@ export default function Publishers(): JSX.Element {
   const [hasS21, setHasS21]                           = useState<boolean>(false)
   const [queryString, setQueryString]                 = useState<string>('')
   const [publicCongregations, setPublicCongregations] = useState<PublicCongregationModel[]>([])
+  const [sortedPublishers, setSortedPublishers]       = useState<publisherTableData>({
+    publishers: [],
+  })
+  const [sortOrder, setSortOrder]                     = useState<'ASC' | 'DESC'>('ASC')
+  const [sortBy, setSortBy]                           = useState<string>('lastname')
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQueryString(e.target.value)
@@ -40,6 +50,18 @@ export default function Publishers(): JSX.Element {
         setPublishers(result)
       })
   }
+
+  useEffect(() => {
+    let sorted: PublisherModel[] = []
+
+    if (sortOrder === 'ASC') {
+      sorted = publishers.sort((a, b) => (a[sortBy] || '').localeCompare(b[sortBy] || ''))
+    }
+    else {
+      sorted = publishers.sort((a, b) => (b[sortBy] || '').localeCompare(a[sortBy] || ''))
+    }
+    setSortedPublishers({ ...sortedPublishers, publishers: sorted })
+  }, [publishers, sortOrder, sortBy])
 
   const getServiceGroups = (): void => {
     window.electron.ipcRenderer.invoke('get-serviceGroups').then((result: ServiceGroupModel[]) => {
@@ -120,17 +142,94 @@ export default function Publishers(): JSX.Element {
         <Table bleed dense grid sticky striped className="mt-2 [--gutter:theme(spacing.6)] sm:[--gutter:theme(spacing.8)]">
           <TableHead>
             <TableRow>
-              <TableHeader></TableHeader>
-              <TableHeader>{t('publishers.header.group')}</TableHeader>
-              <TableHeader>{t('publishers.header.lastname')}</TableHeader>
-              <TableHeader>{t('publishers.header.firstname')}</TableHeader>
-              <TableHeader>{t('publishers.header.mobile')}</TableHeader>
-              <TableHeader>{t('publishers.header.email')}</TableHeader>
+              <TableHeader>
+              </TableHeader>
+              <TableHeader>
+                <div className="flex justify-between">
+                  {t('publishers.header.group')}
+                </div>
+              </TableHeader>
+              <TableHeader onClick={() => { setSortBy('lastname') }}>
+                <div className="flex justify-between">
+                  {t('publishers.header.lastname')}
+                  <ChevronDownIcon
+                    onClick={() => {
+                      setSortOrder((val) => {
+                        return val === 'ASC'
+                          ? 'DESC'
+                          : 'ASC'
+                      })
+                    }}
+                    className={clsx([
+                      'size-5 transition-transform duration-200 ease-out hover:opacity-90',
+                      (sortBy !== 'lastname') && 'opacity-25',
+                      (sortBy === 'lastname' && sortOrder === 'DESC') && 'rotate-180',
+                    ])}
+                  />
+                </div>
+              </TableHeader>
+              <TableHeader onClick={() => { setSortBy('firstname') }}>
+                <div className="flex justify-between">
+                  {t('publishers.header.firstname')}
+                  <ChevronDownIcon
+                    onClick={() => {
+                      setSortOrder((val) => {
+                        return val === 'ASC'
+                          ? 'DESC'
+                          : 'ASC'
+                      })
+                    }}
+                    className={clsx([
+                      'size-5 transition-transform duration-200 ease-out hover:opacity-90',
+                      (sortBy !== 'firstname') && 'opacity-25',
+                      (sortBy === 'firstname' && sortOrder === 'DESC') && 'rotate-180',
+                    ])}
+                  />
+                </div>
+              </TableHeader>
+              <TableHeader onClick={() => { setSortBy('mobile') }}>
+                <div className="flex justify-between">
+                  {t('publishers.header.mobile')}
+                  <ChevronDownIcon
+                    onClick={() => {
+                      setSortOrder((val) => {
+                        return val === 'ASC'
+                          ? 'DESC'
+                          : 'ASC'
+                      })
+                    }}
+                    className={clsx([
+                      'size-5 transition-transform duration-200 ease-out hover:opacity-90',
+                      (sortBy !== 'mobile') && 'opacity-25',
+                      (sortBy === 'mobile' && sortOrder === 'DESC') && 'rotate-180',
+                    ])}
+                  />
+                </div>
+              </TableHeader>
+              <TableHeader onClick={() => { setSortBy('email') }}>
+                <div className="flex justify-between">
+                  {t('publishers.header.email')}
+                  <ChevronDownIcon
+                    onClick={() => {
+                      setSortOrder((val) => {
+                        return val === 'ASC'
+                          ? 'DESC'
+                          : 'ASC'
+                      })
+                    }}
+                    className={clsx([
+                      'size-5 transition-transform duration-200 ease-out hover:opacity-90',
+                      (sortBy !== 'email') && 'opacity-25',
+                      (sortBy === 'email' && sortOrder === 'DESC') && 'rotate-180',
+                    ])}
+                  />
+                </div>
+              </TableHeader>
               <TableHeader></TableHeader>
             </TableRow>
           </TableHead>
-          <TableBody>
-            {publishers.map((publisher) => {
+          <TableBody data-sort={`${sortBy}-${sortOrder}`}>
+            {sortedPublishers.publishers.map((publisher) => {
               const serviceGroup = serviceGroups.find(sg => sg._id === publisher.serviceGroupId)
               const appointments
                   = publisher.appointments.map((a) => {
